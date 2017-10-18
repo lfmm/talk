b7f64d91b5391abe3cb2217e8303d84c5024df11
Angular CLI <angular-cli@angular.io>
2017-10-18 16:48:20 +0100

    chore: initial commit from @angular/cli

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'is-track-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'is-track';
+}
